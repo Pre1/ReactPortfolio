82cf7471892e28bbba885f6b6f6f3ff4e5f3f37c
abdullah <anabanmi@hotmail.com>
2019-03-06 14:07:12 +0300

    fixing some tags

--- src/App.js
+++ src/App.js
@@ -25,11 +25,11 @@ class App extends Component {
 
           <br />
 
-          <div class="svg-wrapper">
+          <div className="svg-wrapper">
             <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
-              <rect class="shape" height="60" width="320" />
+              <rect className="shape" height="60" width="320" />
             </svg>
-            <div class="text">More Info</div>
+            <div className="text">More Info</div>
           </div>
 
           <ul className="icons">  
